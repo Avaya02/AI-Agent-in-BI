957f00a0421ea521e3734db16fc707c3c320befd
akshatg5 <akshatgirdhar5@gmail.com>
2024-12-06 01:07:08 +0530

    feat : not working

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 import OpenAI from "openai";
 import { getSystemPrompt } from "./prompt";
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 app.use(express.json())
@@ -10,25 +10,23 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.post("/template", async (req, res) => {
+app.post("/template", async (req : express.Request, res : express.Response) => {
   const prompt = req.body.prompt;
   try {
     const response = await openai.chat.completions.create({
       model: "gpt-4o-mini",
       messages: [
-        {role : "system",content :"Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra"},
-        {role: "user",content: prompt}
+        { role: "system", content: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra" },
+        { role: "user", content: prompt }
       ],
-      max_tokens : 10,
+      max_tokens: 10,
     });
-    const result = response.choices[0].message.content
-    return res.json({result})
-  } catch (error:any) {
+    const result = response.choices[0].message.content;
+    return res.json({ result });
+  } catch (error: any) {
     console.error("Error with OpenAI API:", error.message);
     return res.status(500).json({ error: "An error occurred while processing your request." });
   }
-  }
-
 });
 
 async function main() {
